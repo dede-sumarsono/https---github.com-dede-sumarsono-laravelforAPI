f434e35d4e72a4ddcf8009540a525d1c13743e46
dede-sumarsono <dede.sumarsono45@gmail.com>
2023-04-04 22:27:31 +0700

    5 Create Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,16 +6,20 @@ use App\Http\Resources\PostDetailResource;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use PhpParser\Node\Stmt\Function_;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::all(); //paket b
         //return response()->json($posts);  
         //return response()->json(['data' => $posts]);  
-        return PostResource::collection($posts);
+        //$posts = Post::with('writer:id,username')->get(); paket a collection perbedaan loadmissing dan with
+        //return PostDetailResource::collection($posts); paket a collection
+        return PostDetailResource::collection($posts->loadMissing('writer:id,username')); //paket b
+
     }
     
     public function show($id)
@@ -30,5 +34,21 @@ class PostController extends Controller
         $post = Post::findOrFail($id);
         //return response()->json(['data'=>$post]);
         return new PostDetailResource($post);
+    }
+
+    public function store(Request $request)
+    {
+
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required',
+        ]);
+     
+        //return response()->json('Oke bisa diakses method store');
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+        
     }
 }
